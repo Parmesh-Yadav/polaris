200eae7579baa3c58378bed0ec746e6d2f3a9834
Parmesh Yadav <parmesh20319@iiitd.ac.in>
2026-01-24 21:16:06 +0530

    add Convex server utilities and authentication configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ import {
   UserButton,
 } from '@clerk/nextjs'
 import { dark } from '@clerk/themes'
+import { Providers } from "@/components/providers";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -34,38 +35,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-      appearance={{
-        theme: dark,
-      }}
-    >
-      <html lang="en" suppressHydrationWarning>
-        <body
-          className={`${inter.variable} ${plexMono.variable} antialiased`}
-        >
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <header className="flex justify-end items-center p-4 gap-4 h-16">
-              <SignedOut>
-                <SignInButton />
-                <SignUpButton>
-                  <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                    Sign Up
-                  </button>
-                </SignUpButton>
-              </SignedOut>
-              <SignedIn>
-                <UserButton />
-              </SignedIn>
-            </header>
-            {children}
-          </ThemeProvider>
-        </body>
-      </html>
-    </ClerkProvider>
+
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${inter.variable} ${plexMono.variable} antialiased`}
+      >
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+    </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,33 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 const Page = () => {
+  const projects = useQuery(api.projects.get);
+
+  const createProject = useMutation(api.projects.create);
+
   return (
-    <div>
-      <Button>
-        Click me
+    <div className="flex flex-col gap-2 p-4">
+      <Button
+        onClick={() => createProject({ name: "New Project" })}
+      >
+        Add new
       </Button>
+      {
+        projects?.map((project) => (
+          <div key={project._id} className="border rounded flex flex-col p-2">
+            <p>
+              {project.name}
+            </p>
+            <p>
+              {project.ownerId}
+            </p>
+          </div>
+        ))
+      }
     </div>
   );
 }
